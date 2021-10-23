28ac1ac65dcb05bc7e94ba880fd624db212de284
eduardo-ridian <eduardo@ridian.io>
2021-10-23 13:58:03 -0500

    .

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('backpy/requirements.txt') as f:
 
 setup(
     name='backpy',
-    version='0.0.7',
+    version='0.0.8',
     author='Ridian',
     author_email='contact@ridian.io',
     description='Python vectorized backtester',
